db0bb7afc847efebf71464d08771fb11c9ff48e6
grafouni <virachevmatvey@gmail.com>
2019-10-12 13:39:31 +0500

    fixed buzzers and compiling errors

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,7 +1,6 @@
 #include "pitches.h"
 #include "button.h"
 #include "buzzer.h"
-#include <Arduino.h>
 
 #define PIN_BUZZER1 4
 #define PIN_BUZZER2 5
@@ -19,6 +18,7 @@ Buzzer secondBuzzer(PIN_BUZZER2);
 int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};
 double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
 int melodyLength = 8;
+bool hasGameEnded=true;
 
 void setup() {
     firstBuzzer.setMelody(notes, durations, melodyLength);
@@ -29,27 +29,40 @@ void setup() {
 }
 
 void loop() {
- set_rgb (0, 0, 0);
- int waitingTime = random(300, 2500);
- delay(waitingTime);
- set_rgb(255, 0, 0);
- while (1){
-   if (firstButton.wasPressed){
-      firstBuzzer.playSound();
-      break;
-   // delay (2500);
-   }
-   if (secondButton.wasPressed){
-     secondBuzzer.playSound();
-     break; 
- //  delay (2500);
-   } 
+ if(hasGameEnded){
+  gameEnd();
+  gameStart();
   }
-  set_rgb(0, 255, 0);
-  delay (2500);
-  set_rgb(0, 0, 255);
-  delay (500);
+    else {
+     if (firstPlayerButton.wasPressed()){
+        firstBuzzer.turnSoundOn();
+        firstBuzzer.playSound();
+        hasGameEnded=true;
+        set_rgb(0, 255, 0);
+        delay(2500);
+      }
+     if (secondPlayerButton.wasPressed()){
+       secondBuzzer.turnSoundOn();
+       secondBuzzer.playSound();
+       hasGameEnded=true;
+       set_rgb(0, 255, 0);
+       delay(2500);
+      } 
+    }
 }
+  void gameStart(){
+   hasGameEnded=false;
+   set_rgb (0, 0, 0);
+   int waitingTime = random(300, 2500);
+   delay(waitingTime);
+   set_rgb(255, 0, 0);
+  }
+  void gameEnd(){
+    firstBuzzer.turnSoundOff();
+    secondBuzzer.turnSoundOff();
+    set_rgb(0, 0, 255);
+    delay (1000);
+  }
 void set_rgb (int r, int g, int b){
   analogWrite(R_OUT, 255-r);
   analogWrite(G_OUT, 255-g);
